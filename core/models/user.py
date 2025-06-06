5020a7b8c1540977bae2833b7c811af2fbf9585d
sakashima <sakashimaideal@gmail.com>
2025-06-06 13:27:17 +0800

    created user model

---
+++ core/models/user.py
@@ -0,0 +1,7 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(32), unique=True)
