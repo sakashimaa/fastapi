c81194bda11a2e7e15614dc54417668b2a175fa5
sakashima <sakashimaideal@gmail.com>
2025-06-06 13:20:00 +0800

    alembic install

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
+import uvicorn
+import asyncio
 
+from core.config import settings
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as users_router
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
@@ -30,3 +46,7 @@ def add(a: int, b: int):
         "b": b,
         "result": a + b,
     }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
