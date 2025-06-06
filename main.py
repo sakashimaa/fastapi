f6297f2f2295e6cf352c4d93ac2232c816305526
sakashima <sakashimaideal@gmail.com>
2025-06-06 13:22:20 +0800

    deleted unused imports, no more create all

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 import uvicorn
-import asyncio
 
 from core.config import settings
-from core.models import Base, db_helper
 from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as users_router
@@ -14,9 +11,6 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
 
 
