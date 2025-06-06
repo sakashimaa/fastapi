1df86c189a094c12148a1b7ade28d9819dbb8a9c
sakashima <sakashimaideal@gmail.com>
2025-06-06 13:52:13 +0800

    user relation mixin and profile table

--- core/models/user.py
+++ core/models/user.py
@@ -6,9 +6,11 @@ from sqlalchemy import String
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
