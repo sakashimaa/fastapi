10e585401fb030b48c61704be1d52793b23fe5f5
sakashima <sakashimaideal@gmail.com>
2025-06-06 13:32:11 +0800

    created posts model

---
+++ core/models/post.py
@@ -0,0 +1,16 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, ForeignKey
+
+
+class Post(Base):
+    title: Mapped[str] = mapped_column(String(100))
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+    )
